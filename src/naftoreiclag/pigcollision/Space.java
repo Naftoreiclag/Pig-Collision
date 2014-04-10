452d2e2b8a6a315e7a58f4358c6ea07a940be0ca
Naftoreiclag <naftoreiclag@gmail.com>
2014-04-09 22:48:47 -0700

    no auxillary functions

--- src/naftoreiclag/pigcollision/Space.java
+++ src/naftoreiclag/pigcollision/Space.java
@@ -16,103 +16,69 @@ public class Space
 			
 			boolean collides = false;
 			
+			Vector2d penetration;
+			
 			for(Line line : lines)
 			{
-				if(this.collides(line.a, line.b, circle.loc, circle.rad))
+				////////////////////////
+				
+				
+				
+				// What
+				Vector2d AC = circle.loc.subtract(line.a);
+				Vector2d AB = line.b.subtract(line.a);
+
+				// I don't even
+				double AB_distsq = (AB.a * AB.a) + (AB.b * AB.b);
+				
+				// What is a dot product
+				double AC_dot_AB = (AC.a * AB.a) + (AC.b * AB.b);
+				
+				// What is this
+				double magic = AC_dot_AB / AB_distsq;
+
+				// Past B
+				if(magic > 1)
 				{
-					collides = true;
-					break;
+					continue;
+				}
+				
+				// Past A
+				else if(magic < 0)
+				{
+					double AC_distsq = AC.magnitudeSquared();
+					
+					if(AC_distsq <= circle.radsq)
+					{
+						collides = true;
+						break;
+					}
+				}
+				
+				// Within the line
+				else
+				{
+					if(line.a.add(AB.multiply(magic)).distanceSquared(circle.loc) <= circle.radsq)
+					{
+						collides = true;
+						break;
+					}
 				}
 			}
 			
 			if(collides)
 			{
 				circle.loc.subtractLocal(circle.velocity);
+				
+				
 			}
 		}
 	}
 	
 	// I had to use uppercase letters here to avoid (more) confusion
-	public Vector2d closestPoint(Vector2d A, Vector2d B, Vector2d C)
-	{
-		// What
-		Vector2d AC = C.subtract(A);
-		Vector2d AB = B.subtract(A);
-		
-		// I don't even
-		double AB_distsq = (AB.a * AB.a) + (AB.b * AB.b);;
-		
-		// What is a dot product
-		double AC_dot_AB = (AC.a * AB.a) + (AC.b * AB.b);
-		
-		// What is this
-		double magic = AC_dot_AB / AB_distsq;
-		
-		// Get the closest point on an INFINITE line
-		Vector2d returnVal = A.add(AB.multiplyLocal(magic));
-		
-		// Clamp the values to be within the points
-		if(returnVal.a < (A.a < B.a ? A.a : B.a))
-		{
-			return A;
-		}
-		if(returnVal.a > (A.a > B.a ? A.a : B.a))
-		{
-			return B;
-		}
-		
-		System.out.println("Closest:\t" + (int) (returnVal.a) + ", " + (int) (returnVal.b));
-		System.out.println("Circle: \t" + (int) (C.a) + ", " + (int) (C.b));
-		System.out.println("Distsq \t" + (int) (returnVal.distanceSquared(C)));
-		System.out.println("Distance \t" + (int) (Math.sqrt(returnVal.distanceSquared(C))));
-		
-		// Return this beast
-		return returnVal;
-	}
-	
-	
-	// I had to use uppercase letters here to avoid (more) confusion
-	public double pntDistanceSq(Vector2d A, Vector2d B, Vector2d C)
-	{
-		// What
-		Vector2d AC = C.subtract(A);
-		Vector2d AB = B.subtract(A);
-		
-		// I don't even
-		double AB_distsq = AB.magnitudeSquared();
-		
-		// What is a dot product
-		double AC_dot_AB = (AC.a * AB.a) + (AC.b * AB.b);
-		
-		// What is this
-		double magic = AC_dot_AB / AB_distsq;
-		
-		// Get the closest point on an INFINITE line
-		Vector2d returnVal = A.add(AB.multiplyLocal(magic));
-		
-		return returnVal.distanceSquared(C);
-	}
-	
-	// I had to use uppercase letters here to avoid (more) confusion
 	public boolean collides(Vector2d A, Vector2d B, Vector2d C, double rad)
 	{
-		/*
-		// If the x values are the same, then the line is vertical
-		if(A.a == B.a)
-		{
-			// Just check the differences in x
-			return Math.abs(A.a - C.a) <= rad;
-		}
-		
-		// If the y values are the same, then the line is horizontal
-		if(A.b == B.b)
-		{
-			// Just check the differences in y
-			return Math.abs(A.b - C.b) <= rad;
-		}
-		*/
-		
-		// Line is "non-axial" (i.e. not 0, 90, 180, or 270)
+		// TODO: put checks for horizontal / vertical lines for max efficiency over 9000
 		
 		// What
 		Vector2d AC = C.subtract(A);
@@ -135,10 +101,6 @@ public class Space
 			return AC_distsq <= (rad * rad);
 		}
 		
-		// Get the closest point on an INFINITE line
-		Vector2d returnVal = A.add(AB.multiply(magic));
-		
-		
-		return returnVal.distanceSquared(C) <= (rad * rad);
+		return A.add(AB.multiply(magic)).distanceSquared(C) <= (rad * rad);
 	}
 }
